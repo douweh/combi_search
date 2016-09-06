8280bc590baa9162ca73c88a69b5f5134927a888
Douwe Homans <douwe@avocado.nl>
2016-09-06 11:28:57 +0200

    Added full-text search
    Signed-off-by: Douwe Homans <douwe@avocado.nl>

--- lib/combi_search/entry.rb
+++ lib/combi_search/entry.rb
@@ -1,8 +1,15 @@
 require "active_record"
+require "search_cop"
 
 module CombiSearch
   class Entry < ActiveRecord::Base
+    include SearchCop
+
     self.table_name = 'combi_search_entries'
     belongs_to :searchable, :polymorphic => true
+
+    search_scope :search do
+      attributes :content
+    end
   end
 end
